efacebb90f7724d1da38b3bfb79c415d3b797adf
jagnani73 <yashjagnani73@gmail.com>
2023-10-20 14:07:53 +0530

    feat(web/sign in)

--- packages/web-client/app/(wagmi)/layout.tsx
+++ packages/web-client/app/(wagmi)/layout.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { wagmiConfig } from "@/utils/services/wagmi";
+import { AuthTokenProvider } from "@/utils/store";
 import { WagmiConfig } from "wagmi";
 
 const WagmiLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <AuthTokenProvider>{children}</AuthTokenProvider>
+    </WagmiConfig>
+  );
 };
 
 export default WagmiLayout;
